a921aa36da1684b4853b44a6e8f7f7a2c23f4efc
gleison silveira <gleison@jesusesenhor.jesusesenhor>
2023-12-28 05:08:25 -0300

    backendpleno

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import sourceMapSupport from 'source-map-support'
 import { Ignitor } from '@adonisjs/core/build/standalone'
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
-
+// NEED CONDIFURATION
 new Ignitor(__dirname)
   .httpServer()
   .start()
